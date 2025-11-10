9d1f10126aae9a9e764fe27e4e4beee0a81ffc9d
Haren-Rehan <martismanti@gmail.com>
2025-11-10 09:38:38 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+# multipleassigment
+x = 10
+print(x)
+x = 20
+print(x)
+x = 30
+print(x)
+print("\n","="*20)
+
+x = 10
+print("x = " + str(x))
+x = 20
+print("x = " + str(x))
+x = 30
+print("x = " + str(x))
+print("\n","="*20)
+
+
+
+# tupleassign
+x, y, z = 100, -45, 0
+print('x =',x,
+      'y =',y,
+      'z =',z)
+print("\n","="*20)
+
+a = 2
+b = 5
+a = 3
+a = b
+a = 7
+
+print(a, b)
+print("\n","="*20)
+
+
+
+# changeabletype
+a = 10
+print ("first, variable a has value", a, "and type", type(a))
+a = "ABX"
+print("now, variable a has value", a, "and type", type(a))
+print("\n","="*20)
+
+
+
+
