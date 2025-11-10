82066433a977b9b9144765255d5d716837e9b765
Haren-Rehan <martismanti@gmail.com>
2025-11-10 09:36:21 +0700

    Create variable and assigment.py

---
+++ variable and assigment.py
@@ -0,0 +1,29 @@
+# multipleassigment
+x = 10
+print (x)
+x = 20
+print (x)
+x = 30
+print (x)
+print(type(x))
+
+# tupleassign
+x, y, z = 10, -1, 0
+print('x =',x ,'y =',y ,'z =',z)
+
+a = 2
+b = 5
+a = 3
+a = b
+a = 7
+
+print(a, b)
+print("\n","="*20)
+
+
+# changeabletype
+a = 10
+print ("first, variable a has value", a, "and type", type(a))
+a = "ABX"
+print("now, variable a has value", a, "and type", type(a))
+print("\n","="*20)
